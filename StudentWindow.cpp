76d507f1a4d50231a90f126ab7d7e1a2bd7c20a3
Goutham Jayaraman <birdboygou7@hotmail.com>
2014-04-16 18:54:24 +0800

    Fixed Student Widget

--- StudentWindow.cpp
+++ StudentWindow.cpp
@@ -3,31 +3,29 @@
 StudentWindow::StudentWindow(DatabaseLayer *db) : MainWindow(db, new StudentMainWidget())
 {
     this->studentMainWidget = (StudentMainWidget *)mainWidget;
-    connect(this->studentMainWidget, SIGNAL(selectedLesson(int)), this, SLOT(showLessonWidget(int)));
-    connect(db, SIGNAL(newLessonCreated()), this, SLOT(newLessonCreated()));
-}
-
-void StudentWindow::showLessonWidget(int i)
-{
-    LessonsDB *lessons = LessonsDBController::getDB();
-    Lesson lesson = lessons->getLessons().at(i);
-
     this->lessonWidget = new StudentLessonWidget();
 
-
+    connect(this->studentMainWidget, SIGNAL(selectedLesson(int)), this, SLOT(showLessonWidget(int)));
+    connect(db, SIGNAL(newLessonCreated()), this, SLOT(newLessonCreated()));
 
     connect(this->lessonWidget, SIGNAL(prepared()), this->lessonWidget, SLOT(show()));
     connect(this->lessonWidget, SIGNAL(prepared()), this->studentMainWidget, SLOT(hide()));
-
 
     connect(this->lessonWidget, SIGNAL(transitLessons()), this->studentMainWidget, SLOT(show()));
     connect(this->lessonWidget, SIGNAL(logOut()), this, SLOT(logoff()));
     connect(this->lessonWidget, SIGNAL(transitLessons()), this->lessonWidget, SLOT(hide()));
     connect(this->lessonWidget, SIGNAL(logOut()), this->lessonWidget, SLOT(hide()));
 
-    this->lessonWidget->prepare(&lesson);
     connect(this->lessonWidget->menuWidget->lessonButton, SIGNAL(clicked()), this->lessonWidget, SLOT(transit()));
     connect(this->lessonWidget->menuWidget->logoutButton, SIGNAL(clicked()), this->lessonWidget, SLOT(exit()));
+}
+
+void StudentWindow::showLessonWidget(int i)
+{
+    LessonsDB *lessons = LessonsDBController::getDB();
+    Lesson lesson = lessons->getLessons().at(i);
+    this->lessonWidget->prepare(&lesson);
+
 }
 
 void StudentWindow::newLessonCreated()
